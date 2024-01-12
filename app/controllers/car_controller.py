0126edf78900b2acf5af70fa6d287bc84a5ff0ba
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-12 09:51:00 +0300

    Complete car_controller

--- app/controllers/car_controller.py
+++ app/controllers/car_controller.py
@@ -4,32 +4,118 @@ from app.models.car_model import Car
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 
+# Set up basic logging configuration
 logging.basicConfig(level=logging.INFO)
 
+
 def handle_error(e, status_code):
-    logging.error(str(e))
-    return jsonify({'error' : str(e)}), status_code
+    """
+    Centralized error handling function.
+
+    Args:
+    - e: Exception object
+    - status_code: HTTP status code
+
+    Returns:
+    - JSON response containing the error message and the provided status code
+    """
+    error_message = str(e) if not hasattr(e, 'message') else e.message
+    logging.error(f"Error: {error_message}")
+    return jsonify({'error': error_message}), status_code
+
 
 def create_car():
+    """
+    Endpoint for creating a new car entry.
+
+    Returns:
+    - JSON response with the serialized new car entry and HTTP status code
+    """
     try:
+        # Get JSON data from the request
         data = request.get_json()
 
+        # Check for required fields in the data
         if 'make' not in data or 'model' not in data or 'year' not in data or 'user_id' not in data:
             return handle_error('Missing data fields', 400)
-        
-        new_car = Car(make=data['make'], model=data['model'], year=data['year'], user_id=data['user_id'])
+
+        # Create a new Car instance
+        new_car = Car(make=data['make'], model=data['model'],
+                      year=data['year'], user_id=data['user_id'])
+
+        # Add the new car to the database session and commit changes
         db.session.add(new_car)
         db.session.commit()
+
+        # Log and return the serialized new car entry
         logging.info(jsonify(new_car.serialize()))
         return jsonify(new_car.serialize()), 201
-    
+
     except SQLAlchemyError as e:
+        # Handle database-related errors
         return handle_error(e, 500)
 
+
 def get_cars():
+    """
+    Endpoint for retrieving all cars.
+
+    Returns:
+    - JSON response with the serialized list of cars and HTTP status code
+    """
     try:
+        # Query all cars from the database
         cars = Car.query.all()
+
+        # Return the serialized list of cars
         return jsonify([car.serialize() for car in cars]), 200
-    
+
     except SQLAlchemyError as e:
+        # Handle database-related errors
         return handle_error(e, 500)
+
+
+def get_car(car_id):
+    try:
+        car = Car.query.filter_by(id=car_id).first()
+
+        return jsonify(car.serialize()), 200
+
+    except SQLAlchemyError as e:
+        return handle_error(e, 400)
+
+
+def delete_car(car_id):
+    try:
+        car = Car.query.filter_by(id=car_id).first()
+
+        db.session.delete(car)
+        db.session.commit()
+
+        response_body = {
+            "success": True,
+            "message": "Car deleted"
+        }
+
+        return jsonify(response_body), 200
+
+    except SQLAlchemyError as e:
+        return handle_error(e, 400)
+    
+
+def update_car(car_id):
+    try:
+        updated_data = request.get_json()
+
+        car = Car.query.filter_by(id=car_id).first()
+
+        for attr in updated_data:
+            setattr(car, attr, updated_data.get(attr))
+
+        # db.session.add()
+        db.session.commit()
+
+        return jsonify(car.serialize()), 200
+
+    except SQLAlchemyError as e:
+        return handle_error(e, 400)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from app.controllers.user_controller import create_user, get_users, get_user, update_user, delete_user
-from app.controllers.car_controller import create_car, get_cars
+from app.controllers.car_controller import create_car,  get_cars, get_car, update_car, delete_car
 
 bp = Blueprint('bp', __name__)
 
@@ -9,14 +9,18 @@ bp = Blueprint('bp', __name__)
 def home():
     return "Car_User API"
 
+
 @bp.route('/users', methods=['GET'])
 def read_users():
     return get_users()
+
+# !USER CONTROLLERS
 
 
 @bp.route('/users/<int:user_id>', methods=['GET'])
 def read_user(user_id):
     return get_user(user_id)
+
 
 @bp.route('/users', methods=['POST'])
 def post_user():
@@ -32,6 +36,8 @@ def patch_user(user_id):
 def remove_user(user_id):
     return delete_user(user_id)
 
+# !CAR CONTROLLERS
+
 
 @bp.route('/cars', methods=['POST'])
 def add_car():
@@ -41,3 +47,18 @@ def add_car():
 @bp.route('/cars', methods=['GET'])
 def list_cars():
     return get_cars()
+
+
+@bp.route('/cars/<int:car_id>', methods=['GET'])
+def list_car(car_id):
+    return get_car(car_id)
+
+
+@bp.route('/cars/<int:car_id>', methods=['PATCH'])
+def patch_car(car_id):
+    return update_car(car_id)
+
+
+@bp.route('/cars/<int:car_id>', methods=['DELETE'])
+def remove_car(car_id):
+    return delete_car(car_id)
